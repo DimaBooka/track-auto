2618dd7f12f8fe19fb6551ba61c664fd27312bc5
nikhil <nikhil@blowhorn.com>
2017-09-27 14:06:10 +0530

    Added cost parameter to chart option.

--- src/app/trip/components/summary/summary.component.ts
+++ src/app/trip/components/summary/summary.component.ts
@@ -21,13 +21,14 @@ export class SummaryComponent implements OnInit {
 
   private firstOptions: any[] = [
     { value: "trips", label: "No. of trips"},
-    { value: "distance", label: "Distance travelled"}
+    { value: "distance", label: "Distance travelled"},
+    { value: "cost", label: "Cost"},
   ];
   private secondOptions: any[] = [
     { value: "today", label: "Today"},
     { value: "week", label: "This week"},
     { value: "month", label: "This month"},
-    { value: "year", label: "This year"}
+    { value: "year", label: "This year"},
   ];
 
   constructor(private _tripsService: TripsService) {

a66e12f1e17187d2b580bb528e2f61528d5c85d8
DimaBooka <atlasb777@gmail.com>
2017-10-30 01:09:23 +0200

    changed driver info view

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,4 @@
-//export const API_URL = "https://blowhorntest.appspot.com/user/test";
+// export const API_URL = "https://blowhorntest.appspot.com/user/test";
 //export const API_URL = "http://localhost:8080";
 export const API_URL = "/user/v7";
 export const API_URL_LOC = "/assets";
